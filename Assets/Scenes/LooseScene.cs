116b60ef4f39947f2ff75d45b48b8ee6725be3de
Vladislav <dragonvlad@bk.ru>
2023-07-24 18:03:55 +0300

    first commit

---
+++ Assets/Scenes/LooseScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LooseScene : MonoBehaviour
+{
+
+    public void ToSampleScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class Enemy : MonoBehaviour
+{
+    public Slider slider;
+    public GameObject player;
+    private NavMeshAgent agent;
+    private Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        target = player.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.position);
+
+        if ((player.transform.position - agent.transform.position).sqrMagnitude < 10)
+        {
+            slider.value = slider.value - 10 * Time.deltaTime;
+        }
+
+        if (slider.value<=0) {
+            SceneManager.LoadScene("LooseScene");
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Arrow")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
